50bd3e11acb617dd7271c276a09a9422eebaa6fc
Annabella <oav2008@gmail.com>
2024-08-01 11:06:47 -0400

    generated a password and copy it to the clipboard

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_list = password_letters + password_numbers + password_symbols
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, f'{password}]')
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    if len(website_entry.get()) == 0 or len(password_entry.get()) == 0 :
+    if len(website_entry.get()) == 0 or len(password_entry.get()) == 0:
         messagebox.showinfo(title='Oops', message='Please dont leave any fields empty!')
     else:
-        is_ok = messagebox.askokcancel(title=website_entry.get(), message=f"There are details you have entered: \nEmail: {username_entry.get()} \nPassword: {password_entry.get()} \nIs it ok to save?")
+        is_ok = messagebox.askokcancel(title=website_entry.get(),
+                                       message=f"There are details you have entered: \nEmail: {username_entry.get()} \nPassword: {password_entry.get()} \nIs it ok to save?")
         if is_ok:
-                with open("data.txt", 'a') as f:
-                    f.write(f"{website_entry.get()} | {username_entry.get()} | {password_entry.get()} \n")
-                    website_entry.delete(0, 'end')
-                    username_entry.delete(0, 'end')
-                    password_entry.delete(0, 'end')
+            with open("data.txt", 'a') as f:
+                f.write(f"{website_entry.get()} | {username_entry.get()} | {password_entry.get()} \n")
+                website_entry.delete(0, 'end')
+                username_entry.delete(0, 'end')
+                password_entry.delete(0, 'end')
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -24,7 +48,6 @@ window.title("Password Manager")
 window.minsize()
 window.config(padx=50, pady=50)
 
-canvas = Canvas()
 canvas = Canvas(width=200, height=200, highlightthickness=0, )
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
@@ -54,7 +77,7 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky='w')
 password_entry.focus()
 # Generate Password Button
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=password_generator)
 generate_password_button.grid(row=3, column=2)
 
 # Add button
